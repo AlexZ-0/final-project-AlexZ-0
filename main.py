8c865c822951ac22795c55354c223dd473aca054
Alex <azhou5@cps.edu>
2026-05-18 12:25:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+name = input("Name your character: ")
+job = int("Choose your class(Enter corresponding number):\n1)Warrior\n2)Mage\n3)Rogue")
+if job == 1:
+  
+if job == 2:
+  
+if job == 3:
+  
 

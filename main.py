036b23374af04502957c88f949cd293a98acd973
Alex <azhou5@cps.edu>
2026-05-19 16:11:00 +0000

    s

--- main.py
+++ main.py
@@ -29,6 +29,15 @@ job = int(input("Choose your class (Enter corresponding number):\n1)Warrior\n2)M
 
 if job == 1:
     player.inventory.add("Iron_ring")
+    player_Patk = 10
+    player_Matk = 30
+    player_Mhp = 120
+    player_Mmn = 30
+    player_Pdef = 15
+    player_Mdef = 10
+    player_spd = 20
+    player_cr = 15
+    player_cd = 50
 elif job == 2:
     player.inventory.add("Crystal_ring")
 elif job == 3:

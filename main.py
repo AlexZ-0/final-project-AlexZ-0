1924911eb40d1f8e241c439ee0af9508c5a0686d
Alex <azhou5@cps.edu>
2026-05-18 21:41:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import random
+player_hp = 100
+player_mn = 100
+player_def = 5
+player_spd = 50
 class Inventory:
     def __init__(self):
         self.items = []
@@ -12,15 +16,25 @@ class Inventory:
             print("  (empty)")
         else:
             for item in self.items:
-print(f"- {item}")name = input("Name your character: ")
-job = int("Choose your class(Enter corresponding number):\n1)Warrior\n2)Mage\n3)Rogue")
+                print(" ", item)
+
+class Player:
+    def __init__(self):
+        self.inventory = Inventory()
+
+player = Player()
+
+name = input("Enter character name: ")
+job = int(input("Choose your class (Enter corresponding number):\n1)Warrior\n2)Mage\n3)Rogue\n"))
+
 if job == 1:
-  player.inventory.add("Iron_ring")
+    player.inventory.add("Iron_ring")
 elif job == 2:
-  player.inventory.add("Crystal_ring")
+    player.inventory.add("Crystal_ring")
 elif job == 3:
-  player.inventory.add("Gold_ring")
+    player.inventory.add("Gold_ring")
 else:
-  print("Invalid choice")
+    print("Invalid choice")
+
 print(name)
 player.inventory.show()
